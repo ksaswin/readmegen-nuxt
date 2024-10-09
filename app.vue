c478499d72fc6364e389d6a8fce0017c6294af59
K S Aswin <aswin2000.ak@gmail.com>
2024-10-10 00:17:40 +0530

    Add custom styles for markdown render preview elements

--- app.vue
+++ app.vue
@@ -13,4 +13,62 @@ body {
 a {
   @apply text-[#1E66F5] dark:text-[#89B4FA];
 }
+
+.markdown-preview {
+  * {
+    @apply mb-1;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    @apply font-semibold;
+  }
+
+  h1, h2, h3 {
+    @apply border-b border-[#BCC0CC] dark:border-[#45475A];
+  }
+
+  h1, h2, h3, h4 {
+    @apply my-4 pb-2;
+  }
+
+  h1 {
+    @apply text-3xl;
+  }
+
+  h2 {
+    @apply text-2xl;
+  }
+
+  h3 {
+    @apply text-xl;
+  }
+
+  p {
+    @apply mb-4;
+  }
+
+  code {
+    @apply px-2 py-1 rounded text-sm bg-[#DCE0E8] dark:bg-[#45475A];
+  }
+
+  pre {
+    @apply w-full mb-4;
+  }
+
+  pre > code {
+    @apply p-4 block w-full overflow-scroll;
+  }
+
+  ul, ol {
+    @apply ml-9 mb-4;
+  }
+
+  ul li {
+    @apply list-disc;
+  }
+
+  ol li {
+    @apply list-decimal;
+  }
+}
 </style>
